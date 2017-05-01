8f3d5dcad9f1d625210ee3c60056008cfb6b4e06
Kira <kirahaibane@gmail.com>
2017-05-01 04:26:20 +0300

    Update Lowadi_Multi-Account_Sausage_Farm.user.js

--- Lowadi_Multi-Account_Sausage_Farm.user.js
+++ Lowadi_Multi-Account_Sausage_Farm.user.js
@@ -8,7 +8,7 @@
 // @match       https://www.lowadi.com/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @require	https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.4/js.cookie.min.js
-// @downloadURL https://github.com/Archer-Dante/scriptlands/raw/master/Lowadi_Multi-Account_Sausage_Farm.user.js
+// @downloadURL https://github.com/Haibane-Kira/Script-Dumpster/raw/master/Lowadi_Multi-Account_Sausage_Farm.user.js
 // ==/UserScript==
 
 
